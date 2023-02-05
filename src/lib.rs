a493aa5c1a282f3034bb0fd35e23b3fc77d4416b
Lowell Thoerner <lowellashton@gmail.com>
2023-02-04 22:46:03 -0800

    Re-added extended description.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
+//! This crate provides a simple interface for creating arbitrarily-sized memory blocks.
+//! This can be used for testing, benchmarking, or just for fun.
+//! The crate is named "walloc" because it is malloc's Wario.
+
 pub mod mem;
